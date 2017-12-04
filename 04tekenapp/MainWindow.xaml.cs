ab6cc36326af233b294f2d5b100120025240621a
davidgithubby <31918922+davidgithubby@users.noreply.github.com>
2017-12-04 13:25:38 +0100

    first commit

--- 04tekenapp/MainWindow.xaml.cs
+++ 04tekenapp/MainWindow.xaml.cs
@@ -24,5 +24,97 @@ namespace _04tekenapp
         {
             InitializeComponent();
         }
+
+        private bool radioButtonFunction(Shape shapeArg)
+        {
+            if (radioButtonCyan.IsChecked == true)
+            {
+                shapeArg.Stroke = System.Windows.Media.Brushes.Cyan;
+                return true;
+            }
+            else if (radioButtonYellow.IsChecked == true)
+            {
+                shapeArg.Stroke = System.Windows.Media.Brushes.Yellow;
+                return true;
+            }
+            else if (radioButtonRed.IsChecked == true)
+            {
+                shapeArg.Stroke = System.Windows.Media.Brushes.Red;
+                return true;
+            }
+            return false;
+        }
+   
+        private void btn_reset(object sender, RoutedEventArgs e)
+        {
+            mainGrid.Children.Clear();
+        }
+
+        private void btn_drawLineClick(object sender, RoutedEventArgs e)
+        {
+            Line myLine = new Line();
+
+            if (radioButtonFunction(myLine) == true)
+            {
+                myLine.X1 = 100;
+                myLine.Y1 = 50;
+
+                myLine.X2 = 450;
+                myLine.Y2 = 50;
+
+                myLine.HorizontalAlignment = HorizontalAlignment.Left;
+                myLine.VerticalAlignment = VerticalAlignment.Center;
+                if (checkBoxThickStroke.IsChecked == true)
+                {
+                    myLine.StrokeThickness = 6;
+                }
+                else
+                {
+                    myLine.StrokeThickness = 2;
+                }
+                mainGrid.Children.Add(myLine);
+            }
+        }
+
+        private void btn_rectangle(object sender, RoutedEventArgs e)
+        {
+            Rectangle rec = new Rectangle();
+            
+            if (radioButtonFunction(rec) == true)
+            {
+                rec.Height = 200;
+                rec.Width = 300;
+                if (checkBoxThickStroke.IsChecked == true)
+                {
+                    rec.StrokeThickness = 6;
+                }
+                else
+                {
+                    rec.StrokeThickness = 2;
+                }
+                mainGrid.Children.Add(rec);
+            }
+        }
+
+        private void btn_ellipse(object sender, RoutedEventArgs e)
+        {
+            Ellipse elip = new Ellipse();
+
+            if (radioButtonFunction(elip) == true)
+            {
+                elip.Height = 300;
+                elip.Width = 400;
+                if (checkBoxThickStroke.IsChecked == true)
+                {
+                    elip.StrokeThickness = 6;
+                }
+                else
+                {
+                    elip.StrokeThickness = 2;
+                }
+                
+                mainGrid.Children.Add(elip);
+            }
+        }
     }
 }
